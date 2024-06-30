66a9bf5e3b933ca87b7e6f1b2882515684da1fdd
Amit Verma <amitvermaup57@gmail.com>
2024-07-01 00:06:41 +0530

    bug fixing

--- main.js
+++ main.js
@@ -3,16 +3,25 @@ const { startWorker } = require("./bullMQ/worker");
 const prompt = require('prompt-sync')();
 
 async function main() {
-    const val = prompt("choose one option, Press Enter:- \n 1:- Start service on email which from Now \n 2:- Start service previour email as well \n ")
+    const val = prompt("choose one option, Press Enter:- \n 1:- Start service on email which from Now \n 2:- Start service previous email as well \n ")
     if(val == 2 || val == 1){
-    setTimeout(async () => {
+      console.log("before producer");
+      const verify = await producer(val);
+      if(!verify){
+        console.log("No emails");
+        return;
+      }
+      console.log("worker start");
+      await startWorker()
+    setInterval(async () => {
+      console.log("setinterval");
       const verify = await producer(val);
       if(!verify){
         console.log("No emails");
         return;
       }
       await startWorker()
-    },15000);
+    },900000);
   }else{
     console.log("invalid Input :- ", val)
   }
